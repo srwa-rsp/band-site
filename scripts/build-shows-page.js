ac15426ef49359746560e21cdcf9cef089eadee0
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-01 17:59:15 -0400

    feat: add dynamic active class to shows

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,68 +1,120 @@
 const showsContainer = document.querySelector(".shows__list");
 
-
 const showsList = [
-    {date: "Mon Sept 09 2024", venue: "Ronald Lane ", location:"San Francisco, CA"},
-    {date: "Tue Sept 17 2024 ", venue: "Pier 3 East  ", location:"San Francisco, CA"},
-    {date: "Sat Oct 12 2024 ", venue: "View Lounge  ", location:"San Francisco, CA"},
-    {date: "Sat Nov 16 2024 ", venue: "Hyatt Agency  ", location:"San Francisco, CA"},
-    {date: "Fri Nov 29 2024", venue: "Moscow Center  ", location:"San Francisco, CA"},
-    {date: "Wed Dec 18 2024 ", venue: "Press Club ", location:"San Francisco, CA"},
-]
+  {
+    date: "Mon Sept 09 2024",
+    venue: "Ronald Lane ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 17 2024 ",
+    venue: "Pier 3 East  ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Oct 12 2024 ",
+    venue: "View Lounge  ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 16 2024 ",
+    venue: "Hyatt Agency  ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 29 2024",
+    venue: "Moscow Center  ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 18 2024 ",
+    venue: "Press Club ",
+    location: "San Francisco, CA",
+  },
+];
 
-function renderShows(){
-    for(let i = 0 ; i< showsList.length; i++){
-        //ul
-        const showItem = document.createElement("ul")
-        showItem.classList.add("shows__item")
-        showsContainer.appendChild(showItem)
+function renderShows(array) {
+  //hidden labels
+  //   const hiddenLabelsContainer = document.createElement("ul");
+  //   hiddenLabelsContainer.classList.add("shows__item-hidden");
+  //   showsContainer.appendChild(hiddenLabelsContainer);
 
-        //first li -- date
-        const dateElement = document.createElement("li")
-        showItem.appendChild(dateElement)
+  //   const hiddenDateElement = document.createElement("li");
+  //   hiddenDateElement.innerText = "date"
+  //   hiddenDateElement.classList.add("label")
+  //   hiddenLabelsContainer.appendChild(hiddenDateElement)
 
-        const dateLabel = document.createElement("span")
-        dateLabel.innerText = "date";
-        dateLabel.classList.add("shows__label", "label")
-        dateElement.appendChild(dateLabel)
+  //   const hiddenVenueElement = document.createElement("li");
+  //   hiddenVenueElement.innerText = "venue"
+  //   hiddenVenueElement.classList.add("label")
+  //   hiddenLabelsContainer.appendChild(hiddenVenueElement)
 
-        const dateInfo = document.createElement("strong")
-        dateInfo.innerText =  showsList[i].date;
-        dateElement.appendChild(dateInfo)
+  //   const hiddenLocationElement = document.createElement("li");
+  //   hiddenLocationElement.innerText = "location"
+  //   hiddenLocationElement.classList.add("label")
+  //   hiddenLabelsContainer.appendChild(hiddenLocationElement)
 
-        //second li -- venue
-        const venueElement = document.createElement("li")
-        showItem.appendChild(venueElement)
+  for (let i = 0; i < array.length; i++) {
+    //ul
+    const showItem = document.createElement("ul");
+    showItem.classList.add("shows__item");
+    showsContainer.appendChild(showItem);
 
-        const venueLabel = document.createElement("span")
-        venueLabel.innerText = "venue";
-        venueLabel.classList.add("shows__label", "label")
-        venueElement.appendChild(venueLabel)
+    //first li -- date
+    const dateElement = document.createElement("li");
+    showItem.appendChild(dateElement);
 
-        const venueInfo = document.createElement("span")
-        venueInfo.innerText =  showsList[i].venue;
-        venueElement.appendChild(venueInfo)
+    const dateLabel = document.createElement("span");
+    dateLabel.innerText = "date";
+    dateLabel.classList.add("shows__label", "label");
+    dateElement.appendChild(dateLabel);
 
-        //third li -- location
-        const locationElement = document.createElement("li")
-        showItem.appendChild(locationElement)
+    const dateInfo = document.createElement("strong");
+    dateInfo.innerText = array[i].date;
+    dateElement.appendChild(dateInfo);
 
-        const locationLabel = document.createElement("span")
-        locationLabel.innerText = "location";
-        locationLabel.classList.add("shows__label", "label")
-        locationElement.appendChild(locationLabel)
+    //second li -- venue
+    const venueElement = document.createElement("li");
+    showItem.appendChild(venueElement);
 
-        const locationInfo = document.createElement("span")
-        locationInfo.innerText =  showsList[i].location;
-        locationElement.appendChild(locationInfo)
+    const venueLabel = document.createElement("span");
+    venueLabel.innerText = "venue";
+    venueLabel.classList.add("shows__label", "label");
+    venueElement.appendChild(venueLabel);
 
-        //button
-        const buttonElement = document.createElement("button");
-        buttonElement.innerText = "buy ticket";
-        buttonElement.classList.add("button")
-        showItem.appendChild(buttonElement)
-        
-    }
+    const venueInfo = document.createElement("span");
+    venueInfo.innerText = array[i].venue;
+    venueElement.appendChild(venueInfo);
+
+    //third li -- location
+    const locationElement = document.createElement("li");
+    showItem.appendChild(locationElement);
+
+    const locationLabel = document.createElement("span");
+    locationLabel.innerText = "location";
+    locationLabel.classList.add("shows__label", "label");
+    locationElement.appendChild(locationLabel);
+
+    const locationInfo = document.createElement("span");
+    locationInfo.innerText = array[i].location;
+    locationElement.appendChild(locationInfo);
+
+    //button
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = "buy ticket";
+    buttonElement.classList.add("button");
+    showItem.appendChild(buttonElement);
+  }
 }
 
-renderShows()
+renderShows(showsList);
+
+document.querySelectorAll(".shows__item").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    document
+      .querySelectorAll(".shows__item")
+      .forEach((el) => el.classList.remove("active"));
+
+    item.classList.add("active");
+  });
+});
